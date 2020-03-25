bc20655365c0782c92ba5643fe8616f61ddaa4cd
Arthur Dutra Alves <arthurdual2014@gmail.com>
2020-03-25 14:23:21 -0300

    subindo projeto

--- public/js/controllers/gestao-controller.js
+++ public/js/controllers/gestao-controller.js
@@ -1,4 +1,4 @@
-angular.module('pontoeletronico').controller('GestaoController', function ($scope, $http, $localStorage, $routeParams) {
+angular.module('pontoeletronico').controller('GestaoController', function ($scope, $http, $routeParams, $route) {
 	$scope.gestao = []
 	$scope.filtro = ''
 	url = 'https://apirest-pontoeletronico.herokuapp.com/api/gestao/usuario'
@@ -19,29 +19,45 @@ angular.module('pontoeletronico').controller('GestaoController', function ($scop
 				alert('Não foi possível obter as informações deste usuario #' + $routeParams.usuarioId + ', Ocorreu um erro: ' + erro);
 			});
 	}
-	$scope.submeter = function () {
-		if ($scope.formulario.$valid) {// valida formulario
-			if ($scope.novoUsuario.id) {// Ediata um usuario
-				$http.put(url, $scope.novoUsuario)
-					.success(function () {
-						alert('Informações do usuario(a) ' + $scope.novoUsuario.nome + ' alteradas com sucesso');
-					})
-					.error(function (erro) {
-						alert('Não foi possível fazer as alterações no usuario #' + $routeParams.usuarioId + ', Ocorreu um erro: ' + erro);
-					});
 
-			} else {// casdastra novo usuario
-				console.log($scope.novoUsuario);
-				$http.post(url, $scope.novoUsuario)
-					.success(function () {
-						alert('Usuario ' + $scope.novoUsuario.nome + ' cadastrado(a) com sucesso');
-						$scope.novoUsuario = {};
-					})
-					.error(function (erro) {
-						console.log(erro);
-						alert('Não foi possível cadastrar o usuario' + ', Ocorreu um erro: ' + erro);
-					})
+	$scope.submeter = function () {
+		alert('Processando solicitação ... Aguarde!!');
+		if ($scope.formulario.$valid) {// valida formulario
+
+			usuarioExistente = false
+			for (let i = 0; i < $scope.gestao.length; i++) {// valida se o usuario ja existe
+				if ($scope.gestao[i]['nome'] == $scope.novoUsuario.nome) {
+					usuarioExistente = true
+					break
+				}
 			}
+
+			if (usuarioExistente == false) {
+				if ($scope.novoUsuario.id) {// Edita um usuario
+					$http.put(url, $scope.novoUsuario)
+						.success(function () {
+							alert('Informações do usuario(a) ' + $scope.novoUsuario.nome + ' alteradas com sucesso');
+							$route.reload()
+						})
+						.error(function (erro) {
+							alert('Não foi possível fazer as alterações no usuario #' + $routeParams.usuarioId + ', Ocorreu um erro: ' + erro);
+						});
+				} else {// casdastra novo usuario
+					console.log($scope.novoUsuario);
+					$http.post(url, $scope.novoUsuario)
+						.success(function () {
+							alert('Usuario ' + $scope.novoUsuario.nome + ' cadastrado(a) com sucesso');
+							$scope.novoUsuario = {};
+						})
+						.error(function (erro) {
+							console.log(erro);
+							alert('Não foi possível cadastrar o usuario' + ', Ocorreu um erro: ' + erro);
+						})
+				}
+			} else {
+				alert('Este usuario já existe, Por favor insira outro nome de usuario!!')
+			}
+
 		}
 	};
 });
--- public/js/controllers/ponto-controller.js
+++ public/js/controllers/ponto-controller.js
@@ -1,5 +1,5 @@
 angular.module('pontoeletronico')
-	.controller('PontoController', function ($scope, $http, $localStorage, $routeParams) {
+	.controller('PontoController', function ($scope, $http, $localStorage, $routeParams, $route) {
 
 		$scope.ponto = []
 		$scope.nome = $localStorage.nome
@@ -18,34 +18,36 @@ angular.module('pontoeletronico')
 				$scope.ponto = ponto;
 				console.log(ponto)
 
-				//calcula jornada do ultimo dia com ponto registrado
+				//calcula jornada do dia
 				timestampEntrada = []
 				timestampSaida = []
-				dataBase = ponto[0]['data'].split("T")[0]
-				for (const date in $scope.ponto) {
-					if (dataBase = ponto[date]['data'].split("T")[0]) {
-						if (ponto[date]['tipo'] == 'entrada') {
-							entrada = ponto[date]['data'].split("T")[1].split(":")
-							timestampEntrada.push(parseInt(entrada[0] * 3600) + parseInt(entrada[1] * 60) + parseInt(entrada[2]))
-						} else {
-							saida = ponto[date]['data'].split("T")[1].split(":")
-							timestampSaida.push(parseInt(saida[0] * 3600) + parseInt(saida[1] * 60) + parseInt(saida[2]))
+				if ($scope.ponto.length != 0) {// verifica se nao esta vazio 
+					dataBase = ponto[ponto.length - 1]['data'].split("T")[0]
+					for (const date in $scope.ponto) { // separa entradas em um array e saidas em outro
+						if (dataBase = ponto[date]['data'].split("T")[0]) {
+							if (ponto[date]['tipo'] == 'entrada') {
+								entrada = ponto[date]['data'].split("T")[1].split(":")
+								timestampEntrada.push(parseInt(entrada[0] * 3600) + parseInt(entrada[1] * 60) + parseInt(entrada[2]))
+							} else {
+								saida = ponto[date]['data'].split("T")[1].split(":")
+								timestampSaida.push(parseInt(saida[0] * 3600) + parseInt(saida[1] * 60) + parseInt(saida[2]))
+							}
 						}
-					}
-				};
+					};
+				}
 
 				time = 0
-				if (timestampEntrada.length == timestampSaida.length) {// calcula time
-					for (const key in timestampEntrada) {
+				if (timestampEntrada.length == timestampSaida.length) {// verifica se os array tem o msm tamanho
+					for (const key in timestampEntrada) { // calcula jornada
 						time += timestampSaida[key] - timestampEntrada[key]
 					}
-				} else if (timestampEntrada.length == timestampSaida.length + 1) {// calcula time caso em tempo real
-					for (let i = 0; i < timestampSaida.length; i++) {
+				} else if (timestampEntrada.length == timestampSaida.length + 1) {//verifica falta uma saida no array
+					for (let i = 0; i < timestampSaida.length; i++) {// calcula jornada
 						time += timestampSaida[i] - timestampEntrada[i]
 					}
 					d = new Date();
-					n = d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
-					time += n - timestampEntrada[timestampEntrada.length - 1]
+					horasRestantes = d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+					time += horasRestantes - timestampEntrada[timestampEntrada.length - 1] // soma jornada obtendo o horario atual
 				}
 
 				//transforma timestamp da jornada pra time
@@ -59,13 +61,25 @@ angular.module('pontoeletronico')
 			})
 
 		$scope.submeter = function () {// bate ponto
-			$scope.batendoPonto.nome = $scope.nome
-			console.log($scope.batendoPonto)
+			$scope.batendoPonto.nome = $routeParams.usuarioId
+			if ($scope.ponto.length == 0) {// caso seja primeira batida de ponto do dia
+				batePonto()
+			} else {
+				if ($scope.batendoPonto.tipo == $scope.ponto[$scope.ponto.length - 1]['tipo']) { // verifica entrada duplicada
+					alert('Não é possivel bater um ponto ' + $scope.batendoPonto.tipo + ' após resgistrar uma ' + $scope.ponto[$scope.ponto.length - 1]['tipo'])
+				} else {
+					batePonto()
+				}
+			}
+		}
+
+		function batePonto() {
 			if ($scope.batendoPonto.tipo == 'entrada' || $scope.batendoPonto.tipo == 'saida') {
 				$http.post(url, $scope.batendoPonto)
 					.success(function () {
 						$scope.novoUsuario = {};
 						alert('Ponto batido')
+						$route.reload()
 					})
 					.error(function (erro) {
 						alert('Não foi possível bater o ponto tente novamente mais tarde, Ocorreu um erro: ' + erro)
@@ -75,3 +89,4 @@ angular.module('pontoeletronico')
 			}
 		}
 	});
+
